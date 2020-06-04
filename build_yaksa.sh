12fdbc0f5a687503bd14d915f2831ffe1f3012f5
Hui Zhou <hzhou321@anl.gov>
2020-06-04 14:00:28 -0500

    update

--- build_yaksa.sh
+++ build_yaksa.sh
@@ -1,7 +1,5 @@
 rm -rf yaksa
 
-# commit=24cd2f2090665eebe5f8dc997fe5f7f2466acdd5
-# commit=9ea097c7639ee8060d977200a42ac20be9f2a86e
 commit=f4c0ee1b8f147148573b471fe4a6ac3e7644eaba
 
 git clone https://github.com/pmodels/yaksa
