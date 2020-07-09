0a5034f6f762f6452c69f10fcd0cad777c528d01
Hui Zhou <hzhou321@anl.gov>
2020-07-08 20:01:29 -0500

    update libfabric

--- build_libfabric.sh
+++ build_libfabric.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -xe
 
-commit=a2abb351ef6a58af5794757eb7f8d7a141ff2128
+# commit=a2abb351ef6a58af5794757eb7f8d7a141ff2128
+commit=-2a691d7c014080be4ca3e8c8c1cd9492b1dbc9e8
 
 rm -rf libfabric
 
@@ -10,6 +11,6 @@ cd libfabric
 git checkout -b build $commit
 
 sh autogen.sh
-./configure --enable-embedded --disable-verbs
+./configure --enable-embedded
 make
 
