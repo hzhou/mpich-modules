8f8147c9313eceffea6a18d1694baa6a3aaba6ab
Hui Zhou <hzhou321@anl.gov>
2021-02-17 17:29:47 -0600

    update yaksa

--- build_yaksa.sh
+++ build_yaksa.sh
@@ -4,7 +4,8 @@ set -xe
 rm -rf yaksa
 
 # commit=110f306ac5fc63af3a5d21ed63a70e053a4c483a
-commit=be77809a822a3824a928f4ad08a74edc38b8068c
+# commit=be77809a822a3824a928f4ad08a74edc38b8068c
+commit=d44dfa922de0f25958d5cb2094284ff4984b5b5f
 
 git clone https://github.com/pmodels/yaksa
 cd yaksa
