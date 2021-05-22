5edd70b70c381e02b577fc20364aec1b84306ae5
Hui Zhou <hzhou321@anl.gov>
2021-05-22 11:37:53 -0500

    update ucx and libfabric

--- build_hwloc.sh
+++ build_hwloc.sh
@@ -2,7 +2,8 @@
 set -xe
 
 # commit=fb27dc6e21bac14754d1b50b57f752e37d475704
-commit=81f40274a0af59f7b3a152a1bae73fe8df09b968
+# commit=81f40274a0af59f7b3a152a1bae73fe8df09b968
+commit=15a78849f7ea56942fee86cb3e9015ab0973ab54
 
 rm -rf hwloc
 
@@ -12,6 +13,6 @@ git checkout -b build $commit
 
 sh autogen.sh
 export CFLAGS=-fvisibility=hidden
-./configure --enable-embedded-mode --enable-visibility=no
+./configure --enable-embedded-mode --enable-visibility=no --disable-libxml2 --disable-nvml --disable-cuda --disable-opencl --disable-rsmi
 make
 
--- build_ucx.sh
+++ build_ucx.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -xe
 
-commit=c1bac6ebd8d075b34f306fa17ea85330ff71103e
+# commit=c1bac6ebd8d075b34f306fa17ea85330ff71103e
+commit=7fcdf34d37b602dab830360f1835a0c9eae81b0c
 
 rm -rf ucx
 
 git clone https://github.com/pmodels/ucx
 cd ucx
 git checkout -b build $commit
+
+perl -p -i -e 'if (/^if HAVE_UCG/) {$skip = 1} elsif (/^EXTRA_DIST \+=/) {$skip = 0} if ($skip) {goto LINE}' Makefile.am 
 
 sh autogen.sh
 ./configure --prefix=/MODPREFIX --disable-static
