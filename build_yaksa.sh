890be343105fd193e6a3d06db7b750b589b44c75
Hui Zhou <hzhou321@anl.gov>
2021-08-08 11:29:24 -0500

    build_yaksa: add --enable-debug

--- build_yaksa.sh
+++ build_yaksa.sh
@@ -5,16 +5,15 @@ rm -rf yaksa
 
 # commit=d44dfa922de0f25958d5cb2094284ff4984b5b5f
 # commit=0ad6172b9658aef93e10cba6f47e36acf514e8d4
-commit=d4e16de491895bf1e4a3db4722fbd850cfc9bd93
+# commit=d4e16de491895bf1e4a3db4722fbd850cfc9bd93
+commit=e2c20536586216513c27bb1342fd346e7e64e728
 
 git clone https://github.com/pmodels/yaksa
 cd yaksa
 git checkout -b build $commit
 
-# sed -i -e 's/lib_LTLIBRARIES/noinst_LTLIBRARIES/' Makefile.am
-
 sh autogen.sh
 # take 1 optional configure argument, e.g. --with-cuda=/usr/local/cuda-11.1
-./configure --enable-embedded $1
+./configure --enable-embedded --enable-debug $1
 make
 
