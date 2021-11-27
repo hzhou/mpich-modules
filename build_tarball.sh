9f29290f7495a464d5286d81ec1c137b486d028e
Hui Zhou <hzhou321@anl.gov>
2021-11-27 08:51:34 -0600

    update build_tarball.sh

--- build_tarball.sh
+++ build_tarball.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
-set -xe
+set -e
 
 if test -z "$MODULES" ; then
     MODULES="libtool yaksa hwloc ucx libfabric json-c izem"
 fi
 
+echo "Empty build..."
 if test -d build ; then
     rm -rf build/*
 else
     mkdir build
 fi
 
-
+echo "Copy $MODULES..."
 # need preserve the timestamps
-cp -rp $MODULES build/
+for a in $MODULES ; do
+    if test -d $a ; then
+        echo "    $a"
+        cp -rp $a build/
+    fi
+done
 
 cd build
 
+echo "Leanify..."
 # exclude ucx (since it need rebuild on install)
 for dir in $MODULES ; do
     if test ! -d $dir ; then
-        echo Not found: $dir
+        echo "    $dir: not found"
     else
         if test "$dir" != "ucx" ; then
+            echo "    $dir"
             find $dir -name '*.o' |xargs rm -f
         fi
     fi
 done
 
+echo '    */.git'
 rm -rf */.git
-tar czf ../modules.tar.gz $MODULES
+
+echo 'Create modules.tar.gz'
+tar czf ../modules.tar.gz *
+echo '    done.'
