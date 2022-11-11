7b8bea36e770d41962b6cd77ab5907d99edc44b1
Hui Zhou <hzhou321@anl.gov>
2022-11-11 16:29:00 -0600

    update libfabric

--- build_libfabric.sh
+++ build_libfabric.sh
@@ -6,17 +6,14 @@ set -xe
 # commit=ce145450cd19c64dc120ec6723af665498e1c141
 # commit=b2b4e201b68c4992ee1d22812a764c9ad95a96a1
 # commit=15c1dcb14d84eae903a459c63c095c02533dd92d
-commit=be81fb4bd78e768052587adb10f1b14fb24b5bf0
+# commit=be81fb4bd78e768052587adb10f1b14fb24b5bf0
+commit=d451a4f14a11154879dc60177eb9a553137d4ef2
 
 rm -rf libfabric
 
 git clone https://github.com/pmodels/libfabric
 cd libfabric
 git checkout -b build $commit
-
-git remote add my $HOME/work/libfabric
-git fetch my
-git cherry-pick 2d773ea5b
 
 extra_option=
 if test $(uname) = "FreeBSD" ; then
